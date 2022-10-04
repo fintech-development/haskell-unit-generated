13eea9f695fd5209df4fa15ed6d51e098c9969cc
Alexey Simuskov <opiumofthepeople@yandex.ru>
2022-10-04 15:07:09 +0000

    Hotfix: Only Checking product is available

--- lib/TheUnit/Model/Accounts/UnitDepositProduct.hs
+++ lib/TheUnit/Model/Accounts/UnitDepositProduct.hs
@@ -10,9 +10,14 @@ import Data.Aeson.Deriving
 import qualified Data.OpenApi as OpenApi
 import GHC.Generics (Generic)
 
+--  | [Unit deposit product](https://docs.unit.co/deposit-accounts#deposit-products)
+-- The deposit product defines the set of terms that are applied to the account:
+-- Deposit products are created and updated by Unit's compliance team
+--
+-- By default only Checking as available
 data UnitDepositProduct
   = UnitDepositProduct'Checking
-  | UnitDepositProduct'Savings
+  | UnitDepositProduct'UNKNOWN
   deriving (Show, Eq, Generic)
   deriving anyclass (OpenApi.ToSchema)
   deriving
