1323c83f1d70996370619a72458136cb5c9f3626
Alexey Simuskov <opiumofthepeople@yandex.ru>
2023-02-09 19:09:04 +0000

    Updated version of ghc

--- shell.nix
+++ shell.nix
@@ -1,15 +1,23 @@
 #!/usr/bin/env nix-shell
-with import <nixpkgs> {};
+with import <nixos-22.11> {};
 
 let
-  ghc = haskell.compiler.ghc8107;
-  hls = haskell-language-server; # .override { supportedGhcVersions = [ "8107" ]; };
-  ormolu = haskell.packages.ghc8107.ormolu;
-  apply-refact = haskell.packages.ghc8107.apply-refact;
+  ghc = haskell.compiler.ghc925;
+
+  hls = haskell-language-server.override {
+    supportedGhcVersions = [ "92" ];
+    };
+  haskell-packages = haskell.packages.ghc925;
+  ormolu = haskell-packages.ormolu;
+  hpack = haskell-packages.hpack;
+  apply-refact = haskell-packages.apply-refact;
 in
 haskell.lib.buildStackProject {
     name = "hls";
     buildInputs = [
+      zlib
+      xz
+
       hls
       ghc
       stack
